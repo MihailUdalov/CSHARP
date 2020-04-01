15bbb8429722066e208fab521a32bfdfe983f6cb
Удалов Михаил <misha2001dw@gmail.com>
2020-04-01 22:33:55 +0300

    Task1-10

---
+++ Task6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("\t   House: \n"+
+                               "\t     **\n"+
+                               "\t    *  *\n"+
+                               "\t   *    *\n"+
+                               "\t  ********\n"+
+                               "\t * *    * *\n"+
+                               "\t   *    * \n"+
+                               "\t   *    * \n"+
+                               "\t   ****** \n");
+        
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************************\n" +
+                              "* Лабораторная работа № 1         *\n" +
+                              "* Выполнил: студент групы ПА-18-3 *\n" +
+                              "* ФАМИЛИЯ ИМЯ ОТЧЕСТВО            *\n" +
+                              "***********************************\n");
+                         
+
+        Console.ReadKey();
+        
+        }
+    }
+}
